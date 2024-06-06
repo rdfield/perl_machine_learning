d2f83900f566b3424938837280115878ac7cdf32
rdfield <raymond.field@mvine.com>
2024-06-06 10:17:50 +0100

    Create nn_preload_cuda.pl

---
+++ nndl_chapter_3/nn_preload_cuda.pl
@@ -0,0 +1,22 @@
+use Modern::Perl;
+use lib '.';
+use ML::MNIST;
+use Data::Dumper;
+use File::Slurp;
+use JSON;
+use Math::Matrix;
+
+use Math::Random;
+use List::Util qw/shuffle/;
+use Time::HiRes qw(gettimeofday tv_interval);
+use ML::GPU qw(load_network mnist_image_guess);
+
+load_network("test_network_1.json");
+
+my $image = scalar(read_file(shift));
+
+my @btmp = unpack('C*',$image);
+foreach my $b (0 .. $#btmp) {
+   $btmp[$b] = $btmp[$b]/255;
+}
+say join(",",@{mnist_image_guess(\@btmp)});
